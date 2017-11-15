4e62367d590bf393b556658b27bb401e5ccec567
Felix S <xLifeHD@gmail.com>
2017-11-15 08:39:32 +0100

    test

--- Vo1/src/main/Main.java
+++ Vo1/src/main/Main.java
@@ -1,19 +1,22 @@
 package main;
 
 public class Main {
-    
-    public static void main(String[] args) {
-        int i = 0xFFFF_FFFF;
-        int j = 0x0000_0000;
+    public static void main(String[] args) throws Exception {  
+        final char pattern = 'a';         
+        int charCounter = 0;
+        int patternCounter = 0;
         
-        System.out.println(Integer.toBinaryString(i));
-        System.out.println(i);
-        System.out.println(Integer.toBinaryString(j));
-        i = -i;
-        j = -j;
-        System.out.println(Integer.toBinaryString(i));
-        System.out.println(Integer.toBinaryString(j));
-        
+        System.out.println("Bitte Zeichen mit abschliess. <return> eingeben ");
+        int c;
+        while ((c = System.in.read()) != '\r' && c != '\n') {
+            if (c < 'a' || c > 'z')  // count lowercase characters only
+                continue;
+            charCounter++;
+            if (c == pattern)
+                patternCounter++;
+        }
+
+        System.out.println("Anz. char: " + charCounter + ",  Anz. " 
+                            + pattern + ": " + patternCounter);    
     }
-    
 }
